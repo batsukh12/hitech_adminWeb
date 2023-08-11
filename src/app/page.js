edb377fd78e5efabdddf3bb5991b2297d66f81a4
20B1NUM2012 BATSUKH <20B1NUM2012@stud.num.edu.mn>
2023-08-11 11:34:04 +0800

    mui added

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography>hi</Typography>
+    </Box>
+  );
+}
